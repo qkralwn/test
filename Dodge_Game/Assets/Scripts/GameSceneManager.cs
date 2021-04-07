392fabd584921fcac579e9add988a47ca11c3aef
qkralwn <ppomi0661@naver.com>
2021-04-08 00:52:01 +0900

    test

---
+++ Dodge_Game/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour {
+    private Player player;
+    private EnemySpawner spawner;
+    private UI_Time timer;
+
+    public GameObject gameOverUI;
+
+    // Start is called before the first frame update
+    void Start() {
+        timer = FindObjectOfType<UI_Time>();
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<EnemySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (player.GetHP() <= 0) {
+            spawner.OffSpawner();
+            timer.OffTimer();
+
+            gameOverUI.SetActive(true);
+        }
+    }
+
+    public void RestartButton() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitButton() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Dodge_Game/Assets/Scripts/UI_Hp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Hp : MonoBehaviour
+{
+    private Text hpText;
+    private int playerHP = 3; //
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        hpText = GetComponent<Text>();
+        playerHP = player.GetHP(); //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpText.text = "HP : " + player.GetHP();
+    }
+}
